691a2740cc7cb05e825bf40e0ed6a88ef8198918
af5rany <58744028+af5rany@users.noreply.github.com>
2023-06-21 14:36:44 +0300

    added services to categories

--- backend/controllers/category.controller.js
+++ backend/controllers/category.controller.js
@@ -1,38 +1,29 @@
 const Category = require("../models/category.model");
-const AppError = require("../utils/namespace.util");
-// const AppError = require("../lib/errorhandler.lib");
-const { default: categoryService } = require("../services/category.service");
+const AppError = require("../utils/AppError.util");
+const categoryService = require("../services/category.service");
+
 const createCategory = async (req, res, next) => {
-  const category = await categoryService.createCategory(req.body)
-  res.status(201).json({category:category});
+  const category = await categoryService.createCategory(req.body);
+  res.status(201).json({ category: category });
 };
 
 const getAllCategories = async (req, res) => {
-  const categories = await Category.find();
-  res.send(categories);
+  const categories = await categoryService.getAllCategories();
+  res.status(200).json({ categories: categories });
 };
 
 const getCategoryByID = async (req, res) => {
-  const category = await Category.findById(req.params.id);
-  if (!category) {
-    return next(new AppError("THIS CATEGORY IS NOT FOUND", 404));
-  }
-  res.send(category);
+  const category = await categoryService.getCategoryByID(req.params.id);
+  res.status(200).json({ category: category });
 };
 
 const editCategory = async (req, res) => {
-  const category = await Category.findByIdAndUpdate(req.params.id, req.body);
-  if (!category) {
-    return next(new AppError("THIS CATEGORY IS NOT FOUND TO EDIT", 404));
-  }
-  res.send(category);
+  const category = await categoryService.editCategory(req.params.id, req.body);
+  res.status(200).json({ category: category });
 };
 
 const deleteCategory = async (req, res) => {
-  const category = await Category.findByIdAndDelete(req.params.id);
-  if (!category) {
-    return next(new AppError("THIS CATEGORY IS NOT FOUND TO DELETE", 404));
-  }
+  const category = await categoryService.deleteCategory(req.params.id);
   res.status(200).send("Category is Removed Successfully");
 };
 
