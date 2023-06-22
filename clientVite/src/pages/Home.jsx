1c9aff52d6e27bdee6ae931207d341a829261303
Hussein <helnemar@yahoo.com>
2023-06-23 02:29:17 +0300

    navbar & footer

--- clientVite/src/pages/Home.jsx
+++ clientVite/src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import Footer from "../components/common/Footer/Footer";
+import Navbar from '../components/common/NavBar/Navbar';
 import AttractionCard from "../components/HomeContainer/AttractionCard/AttractionCard";
 
 const Home = () => {
   return (
     <>
-      {/* <h1 className="text-3xl font-bold underline"> Hello world! </h1> */}
+      <Navbar />    
       <AttractionCard />
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 };
