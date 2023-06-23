90061ea66c236c9ce684e6934b01351e9adaa98b
mahmoudalmazoon <102832979+mahmoudalmazoon@users.noreply.github.com>
2023-06-23 17:26:38 +0300

    attraction

--- clientVite/src/App.jsx
+++ clientVite/src/App.jsx
@@ -2,11 +2,14 @@ import "./App.css";
 import LoginForm from "./components/common/AuthModal/LoginForm";
 import RegisterForm from "./components/common/AuthModal/registerForm";
 import { Route, Routes, useNavigate } from "react-router-dom";
+import Home from "./pages/Home";
 function App() {
   const navigate = useNavigate();
   return (
     <>
       <Routes>
+
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/register" element={<RegisterForm />} />
       </Routes>
