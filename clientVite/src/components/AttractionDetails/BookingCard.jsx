db4ca2c27273051eefc2eb9b394328145cd2ea3a
asmaa abdrabo <asmaa90@outlook.com>
2023-06-24 17:42:43 +0300

    attraction page

---
+++ clientVite/src/components/AttractionDetails/BookingCard.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Details from "./Details";
+const BookingCard = () => {
+  return (
+    <>
+      <div className="">
+        <div className="max-w-md rounded-3xl p-px bg-gradient-to-b from-blue-300 to-pink-300 dark:from-blue-800 dark:to-purple-800 ">
+          <div className="rounded-[calc(1.5rem-1px)] p-10 bg-white dark:bg-gray-900 text-center">
+            <div className="flex font-bold justify-center">
+              Start From <p className="text-blue-600">&nbsp;100 Dollar&nbsp;</p>{" "}
+              Per Person
+            </div>
+
+            <p className="text-green-600 mt-2">All taxes and fees included</p>
+            <button className="bg-blue-500 mt-3 w-full hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+              Booking Now
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BookingCard;
---
+++ clientVite/src/components/AttractionDetails/Details.jsx
@@ -0,0 +1,115 @@
+import React from "react";
+const Details = () => {
+  return (
+    <>
+      <div className="w-1/2">
+        <p className="font-bold">Top Rate</p>
+        <div className="flex items-center mt-2.5 mb-5">
+          <svg
+            aria-hidden="true"
+            className="w-5 h-5 text-yellow-300"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <title>First star</title>
+            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+          </svg>
+          <svg
+            aria-hidden="true"
+            className="w-5 h-5 text-yellow-300"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <title>Second star</title>
+            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+          </svg>
+          <svg
+            aria-hidden="true"
+            className="w-5 h-5 text-yellow-300"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <title>Third star</title>
+            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+          </svg>
+          <svg
+            aria-hidden="true"
+            className="w-5 h-5 text-yellow-300"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <title>Fourth star</title>
+            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+          </svg>
+          <svg
+            aria-hidden="true"
+            className="w-5 h-5 text-yellow-300"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <title>Fifth star</title>
+            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+          </svg>
+          <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
+            5.0
+          </span>
+        </div>
+        <div>
+          <p>
+            <span className="font-sans text-#004794 font-bold text-2xl text-blue-700">
+              Tour to The East &amp; West Bank of the Nile
+              <span className="text-yellow-500"> Start from $99</span>
+            </span>
+          </p>
+          <p className="mt-5">
+            See the most famous sites around Luxor on both sides of the river in
+            one day. Visit Karnak Temple and Luxor Temple on the East bank and
+            then see the Valley of the Kings and Hatshepsut Temple on the West
+            bank
+          </p>
+          <p className="text-yellow-500 font-bold text-2xl mt-5">Highlights:</p>
+          <ul className="list-disc p-5 mx-3">
+            <li>Discover the hidden treasures of Luxor’s West Bank</li>
+            <li>Visit the tombs of some of Egypt’s greatest kings</li>
+            <li>See the funerary temples and tombs on the Nile’s West Bank.</li>
+          </ul>
+          <p className="text-yellow-500 font-bold text-2xl mt-5">
+            Tour Details:
+          </p>
+          <ul className="list-disc p-5 mx-3">
+            <li>Duration: About 8 Hours</li>
+            <li>Type: Private Tours</li>
+            <li>Run: Everyday</li>
+            <li>Pick-up Time: From 07:30 to 08:00 am</li>
+          </ul>
+          <p className="text-yellow-500 font-bold text-2xl mt-5">
+            What's Include:
+          </p>
+          <ul className="list-disc p-5 mx-3">
+            <li>All transfers by a private air-conditioned vehicle.</li>
+            <li>Pick up services from your hotel & return.</li>
+            <li>Private All Languages speaking Egyptologist guide.</li>
+            <li>Entrance fees to all the mentioned sites.</li>
+            <li>Lunch meal will be served in a local restaurant..</li>
+            <li>Bottled water during your trip.</li>
+            <li>All taxes & service charge.</li>
+          </ul>
+          <p className="text-yellow-500 font-bold text-2xl mt-5">
+            What’s Excluded:
+          </p>
+          <ul className="list-disc p-5 mx-3">
+            <li>Any extras not mentioned in the itinerary.</li>
+            <li>Tipping.</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Details;
