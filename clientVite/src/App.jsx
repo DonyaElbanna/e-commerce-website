e232e444ac509bd3416e33d6f74ce9fada8fd0c6
Donya <donyaelbanna@yahoo.com>
2023-06-24 17:54:17 +0300

    individual city pages

--- clientVite/src/App.jsx
+++ clientVite/src/App.jsx
@@ -4,16 +4,22 @@ import RegisterForm from "./components/common/AuthModal/registerForm";
 import { Route, Routes, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
 import AttractionDetails from "./components/AttractionDetails/AttractionDetails";
+import AttractionsList from "./components/AttractionsList/AttractionsList";
+import Navbar from "./components/common/NavBar/Navbar";
+import Footer from "./components/common/Footer/Footer";
 function App() {
   const navigate = useNavigate();
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/register" element={<RegisterForm />} />
         <Route path="/AttractionDetails" element={<AttractionDetails />} />
+        <Route path="/city/:name" element={<AttractionsList />} />
       </Routes>
+      <Footer />
     </>
   );
 }
