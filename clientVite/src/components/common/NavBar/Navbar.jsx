92263b0a2a7acb5a5ab458dbef9f6bfa66eed7aa
esraa-nassar <esraanassa1997@gmail.com>
2023-06-24 06:19:57 +0300

    footer navbar style

--- clientVite/src/components/common/NavBar/Navbar.jsx
+++ clientVite/src/components/common/NavBar/Navbar.jsx
@@ -1,62 +1,142 @@
-import avatar from "../../../icons/avatar.avif"
-import Style from "./Navbar.module.css"
+import avatar from "../../../icons/avatar.avif";
+import Style from "./Navbar.module.css";
+import gif from "../../../assets/gih.gif";
 const Navbar = () => {
   return (
     <>
-      <div className="navbar bg-black  py-8 px-12">
-        <div className="flex-1">
-          <a style={{fontFamily:'Egypt'}} className="text-4xl text-amber-500 ">EGYPT</a>
-        </div>
-        <div className="flex gap-4">
-          <div className="dropdown dropdown-end">
+      <header className="navbar sticky top-0 z-10 bg-slate-100/80  rounded-b-3xl mb-5 px-4 md:px-10">
+        <aside className="navbar-start">
+          <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost btn-circle">
-              <div className="indicator">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
-                <span className="badge badge-sm indicator-item">8</span>
-              </div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="black"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h7"
+                />
+              </svg>
             </label>
-            <div tabIndex={0} className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow">
-              <div className="card-body">
-                <span className="font-bold text-lg">8 Items</span>
-                <span className="text-info">Subtotal: $999</span>
-                <div className="card-actions">
-                  <button className="btn btn-primary btn-block">View cart</button>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a>Places</a>
+              </li>
+              <li>
+                <a>Category</a>
+              </li>
+              <li>
+                <a>Reviews</a>
+              </li>
+            </ul>
+          </div>
+        </aside>
+        <div className="navbar-center">
+          <img src={gif} className="w-32 hidden md:inline"></img>
+          <p className="logoFont text-yellow-600 md:text-black font-bold text-3xl md:text-4xl mr-16 ">
+            EGYPT
+          </p>
+        </div>
+        <div className="navbar-end">
+          <div className="flex gap-4 items-center">
+            <div className="dropdown dropdown-end -mr-3 md:mr-0">
+              <label tabIndex={0} className="btn btn-ghost btn-circle ">
+                <div className="indicator">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6 md:h-8 md:w-8"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="black"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                    />
+                  </svg>
+                  <span className="badge badge-sm indicator-item">8</span>
+                </div>
+              </label>
+              <div
+                tabIndex={0}
+                className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow"
+              >
+                <div className="card-body">
+                  <span className="font-bold text-lg">8 Items</span>
+                  <span className="text-info">Subtotal: $999</span>
+                  <div className="card-actions">
+                    <button className="btn btn-primary btn-block">
+                      View cart
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
-
-          </div>
-          <div className="dropdown dropdown-end">
-            <label tabIndex={0} className=" btn btn-ghost btn-circle">
-              <div className="indicator">
-                <svg  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
-                </svg>
-                <span className="badge badge-sm indicator-item">0</span>
-              </div>
-            </label>
-          </div>
-
-          <div className="dropdown dropdown-end">
-            <label tabIndex={0} className="w-14 btn btn-ghost btn-circle avatar">
-              <div className="rounded-full">
-                <img src={avatar} />
-              </div>
-            </label>
-            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-              <li>
-                <a className="justify-between">
-                  Profile
-                </a>
-              </li>
-              <li><a>Settings</a></li>
-              <li><a>Logout</a></li>
-            </ul>
+            <div className="dropdown dropdown-end">
+              <label tabIndex={0} className=" btn btn-ghost btn-circle">
+                <div className="indicator">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="black"
+                    className="h-6 w-6 md:h-8 md:w-8"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                    />
+                  </svg>
+                  <span className="badge badge-sm indicator-item">0</span>
+                </div>
+              </label>
+            </div>
+            <div className="dropdown dropdown-end">
+              <label
+                tabIndex={0}
+                className="w-14 btn btn-ghost btn-circle avatar"
+              >
+                <div className="rounded-full">
+                  {/* <img src={avatar} /> */}
+                  <img
+                    src="https://tecdn.b-cdn.net/img/new/avatars/2.webp"
+                    class="w-32 rounded-full"
+                    alt="Avatar"
+                  />
+                </div>
+              </label>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+              >
+                <li>
+                  <a className="justify-between">Profile</a>
+                </li>
+                <li>
+                  <a>Settings</a>
+                </li>
+                <li>
+                  <a>Logout</a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
+      </header>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
