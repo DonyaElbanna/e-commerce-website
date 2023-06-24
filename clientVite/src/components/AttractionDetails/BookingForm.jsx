3848b6442bd92b390c5ab78838d01a27db16fc60
asmaa abdrabo <asmaa90@outlook.com>
2023-06-24 20:19:34 +0300

    booking form

--- clientVite/src/components/AttractionDetails/BookingForm.jsx
+++ clientVite/src/components/AttractionDetails/BookingForm.jsx
@@ -103,7 +103,7 @@ const BookingForm = () => {
                             <input
                               type="tel"
                               id="phone"
-                              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                               placeholder="123-45-678"
                               pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
                               required
