6d4f8fc7a73249dd1c677296fb2fe0b96612302d
af5rany <58744028+af5rany@users.noreply.github.com>
2023-07-01 19:57:26 +0300

    dynamic rating

--- clientVite/src/components/HomeContainer/AttractionCard/AttractionCard.jsx
+++ clientVite/src/components/HomeContainer/AttractionCard/AttractionCard.jsx
@@ -51,6 +51,23 @@ const AttractionCard = ({ attr }) => {
     navigate("/login");
   }
 
+  const starClassNames = [];
+  const rating = 3.3;
+  (function () {
+    const fullStars = Math.floor(rating);
+    const hasHalfStar = rating - fullStars >= 0.5;
+    for (let i = 0; i < fullStars; i++) {
+      starClassNames.push("text-yellow-400");
+    }
+    if (hasHalfStar) {
+      starClassNames.push("text-yellow-400");
+    }
+    const emptyStars = 5 - starClassNames.length;
+    for (let i = 0; i < emptyStars; i++) {
+      starClassNames.push("text-gray-300 dark:text-gray-500");
+    }
+  })();
+
   return (
     <div className="flex justify-center flex-row">
       <Link
@@ -88,9 +105,9 @@ const AttractionCard = ({ attr }) => {
               {attr.category.city}
             </span>
           </div>
-          {/* <span className="text-lg text-gray-900 dark:text-white"></span> */}
+          <span className="text-lg text-gray-900 dark:text-white"></span>
           <div className="flex items-center justify-between">
-            <div className="flex items-center mt-2.5 mb-5">
+            {/* <div className="flex items-center mt-2.5 mb-5">
               <svg
                 aria-hidden="true"
                 className="w-5 h-5 text-yellow-300"
@@ -144,7 +161,23 @@ const AttractionCard = ({ attr }) => {
               <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
                 5.0
               </span>
+            </div> */}
+
+            <div className="flex items-center">
+              {starClassNames.map((className, index) => (
+                <svg
+                  key={index}
+                  aria-hidden="true"
+                  className={`w-5 h-5 ${className}`}
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                </svg>
+              ))}
             </div>
+
             <span className="text-sm text-gray-500">
               {/* <span className=" text-lg text-gray-400">from:</span> */}
               <span className="text-3xl font-bold text-gray-200 dark:text-white">
