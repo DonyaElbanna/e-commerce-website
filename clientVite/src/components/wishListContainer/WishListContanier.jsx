c6b5a8cbcc94ac2d2d4ca2d8584d45cbda522358
af5rany <58744028+af5rany@users.noreply.github.com>
2023-07-03 08:50:16 +0300

    delete from wishlist

--- clientVite/src/components/wishListContainer/WishListContanier.jsx
+++ clientVite/src/components/wishListContainer/WishListContanier.jsx
@@ -2,18 +2,33 @@ import { Container } from "@mui/material";
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 
-const baseURL = "http://localhost:9999/user/64931b6199ee6e4ef036a40f";
+const baseURL = "http://localhost:9999/user/6493210b7b4837976bc3c29e";
 
-const WishListContanier = ({ attraction }) => {
+const WishListContanier = ({ attr }) => {
   const [wishlistItems, setWishlistItems] = useState([]);
   useEffect(() => {
     const getWishlistItems = async () => {
       const { data } = await axios.get(baseURL);
       setWishlistItems(data.wishlist);
     };
+    // const removeFromWishlist = async () => {
+    //   const { data } = await axios.delete(`${baseURL}/`);
+    //   setWishlistItems(data.wishlist);
+    // };
     getWishlistItems();
   }, []);
+  const handleRemoveFromWishlist = async (item) => {
+    const updatedWishlist = wishlistItems.filter(
+      (wishlistItem) => wishlistItem._id !== item._id
+    );
+    setWishlistItems(updatedWishlist);
 
+    // await axios.delete(`${baseURL}/wishlist/${item._id}`);
+    await axios.delete("6493210b7b4837976bc3c29e", { id: item._id });
+    // setIsFilled(!isFilled);
+  };
+
+  // console.log(wishlistItems);
   // const data = wishlistItems.map((item, idx) => {
   //   return (
   //     <tr>
@@ -88,7 +103,10 @@ const WishListContanier = ({ attraction }) => {
 
   const data = wishlistItems.map((item, idx) => {
     return (
-      <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 text-center">
+      <tr
+        key={item._id}
+        className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 text-center"
+      >
         <td className="whitespace-nowrap px-6 py-4 font-medium text-center">
           {idx + 1}
         </td>
@@ -103,18 +121,18 @@ const WishListContanier = ({ attraction }) => {
         <td className="px-6 py-4 text-center">${item.AdultPrice}</td>
         <td className="px-6 py-4  text-centert">
           <div className="flex flex-row justify-center">
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full flex">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full flex">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
-                stroke-width="1.5"
+                strokeWidth="1.5"
                 stroke="currentColor"
                 className="w-6 h-6"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
                 />
               </svg>
@@ -123,9 +141,9 @@ const WishListContanier = ({ attraction }) => {
           </div>
         </td>
         <td className="px-6 py-4 text-center">
-          <a
-            href="#"
-            className="font-medium hover:underline flex justify-center"
+          <button
+            onClick={() => handleRemoveFromWishlist(item)}
+            // className="font-medium hover:underline flex justify-center"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -134,22 +152,22 @@ const WishListContanier = ({ attraction }) => {
               className="w-6 h-6"
             >
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M16.5 4.478v.227a48.816 48.816 0 013.878.512.75.75 0 11-.256 1.478l-.209-.035-1.005 13.07a3 3 0 01-2.991 2.77H8.084a3 3 0 01-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 01-.256-1.478A48.567 48.567 0 017.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 013.369 0c1.603.051 2.815 1.387 2.815 2.951zm-6.136-1.452a51.196 51.196 0 013.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 00-6 0v-.113c0-.794.609-1.428 1.364-1.452zm-.355 5.945a.75.75 0 10-1.5.058l.347 9a.75.75 0 101.499-.058l-.346-9zm5.48.058a.75.75 0 10-1.498-.058l-.347 9a.75.75 0 001.5.058l.345-9z"
-                clip-rule="evenodd"
+                clipRule="evenodd"
               />
             </svg>
             {/* <p>Delete</p> */}
-          </a>
+          </button>
         </td>
       </tr>
     );
   });
   return (
     <div>
-      {/* <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
+      {/* <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
         your
-        <span class="text-transparent bg-clip-text bg-gradient-to-r from-gold-400 to-emerald-600 ">
+        <span className="text-transparent bg-clip-text bg-gradient-to-r from-gold-400 to-emerald-600 ">
           WishList
         </span>
       </h1> */}
@@ -165,22 +183,22 @@ const WishListContanier = ({ attraction }) => {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke-width="1.5"
+          strokeWidth="1.5"
           stroke="brown"
           className="w-12 h-12 mb-8"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
           />
         </svg>
       </div>
 
-      <div className="content-center flex justify-center items-center">
-        <div class="relative overflow-x-auto shadow-md sm:rounded-lg w-10/12">
-          <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+      <div className="content-center flex justify-center items-center mb-36">
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg w-10/12">
+          <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
               <tr>
                 <th scope="col" className="px-6 py-4 text-center">
                   #
