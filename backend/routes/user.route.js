3cdbface769e1aed231a7c553c83e5d74fc66f76
Donya <donyaelbanna@yahoo.com>
2023-07-04 03:06:14 +0300

    blobk/unblock user (admin dash)

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -9,7 +9,7 @@ const {
   getAllUsers,
   toggleWishlist,
   toggleBlock,
-  changeUserRole
+  changeUserRole,
 } = require("../controllers/user.controller");
 
 router.post("", signup);
@@ -24,7 +24,7 @@ router.delete("/:id", deleteUser);
 
 router.post("/:id", toggleWishlist);
 
-router.patch("/block/:id", toggleBlock);
+router.get("/block/:id", toggleBlock);
 
 router.patch("/role/:id", changeUserRole);
 
