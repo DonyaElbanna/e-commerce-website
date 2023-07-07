9631f018870be5846b110bfd10ad7036ed5c7cd1
Donya <donyaelbanna@yahoo.com>
2023-07-08 00:33:00 +0300

    add to whislist for not logged in users

--- clientVite/src/components/wishListContainer/WishListContanier.jsx
+++ clientVite/src/components/wishListContainer/WishListContanier.jsx
@@ -33,83 +33,6 @@ const WishListContanier = ({ attr }) => {
     event.preventDefault();
   };
 
-  // const data = wishlistItems.map((item, idx) => {
-  //   return (
-  //     <div className="flex flex-row align-middle">
-  //       <div
-  //         className="card card-side bg-base-100 shadow-xl w-4/12 ml-64 mb-10"
-  //         key={item._id}
-  //       >
-  //         <figure>
-  //           <img className={wishlistStyle.cardImage} src={item.Images[0]} />
-  //         </figure>
-  //         <div className="card-body w-52 flex justify-between">
-  //           <div>
-  //             <h2 className="card-title">{item.name}</h2>
-  //             <h2 className="card-title">
-  //               <span>Available:</span>
-  //               {item.status}
-  //             </h2>
-  //             <h2 className="card-title justify-between w-32">
-  //               <span>Price:</span>
-  //               <span>{item.AdultPrice}</span>
-  //             </h2>
-  //             <div className="flex mt-5">
-  //               <span className="text-xs inline-flex items-center font-bold leading-sm uppercase px-5 py-1 bg-blue-200 text-blue-700 rounded-full w-min">
-  //                 {/* {item.subcategory} */}
-  //                 Cairo
-  //               </span>
-  //               <span className="text-xs inline-flex items-center font-bold leading-sm uppercase px-4 py-1 mx-3 bg-green-200 text-blue-700 rounded-full w-min">
-  //                 {/* {item.category} */}
-  //                 Cruse
-  //               </span>
-  //             </div>
-  //           </div>
-  //         </div>
-  //       </div>
-  //       <div className=" flex flex-col ml-5 items-center align-middle justify-evenly">
-  //         <div className="flex flex-col justify-center">
-  //           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full flex w-44">
-  //             <svg
-  //               xmlns="http://www.w3.org/2000/svg"
-  //               fill="none"
-  //               viewBox="0 0 24 24"
-  //               strokeWidth="1.5"
-  //               stroke="currentColor"
-  //               className="w-6 h-6"
-  //             >
-  //               <path
-  //                 strokeLinecap="round"
-  //                 strokeLinejoin="round"
-  //                 d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
-  //               />
-  //             </svg>
-  //             <p className="ml-3">Add To Cart</p>
-  //           </button>
-  //         </div>
-  //         <div className="flex">
-  //           <button onClick={() => handleRemoveFromWishlist(item)}>
-  //             <svg
-  //               xmlns="http://www.w3.org/2000/svg"
-  //               viewBox="0 0 24 24"
-  //               fill="red"
-  //               className="w-6 h-6"
-  //             >
-  //               <path
-  //                 fillRule="evenodd"
-  //                 d="M16.5 4.478v.227a48.816 48.816 0 013.878.512.75.75 0 11-.256 1.478l-.209-.035-1.005 13.07a3 3 0 01-2.991 2.77H8.084a3 3 0 01-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 01-.256-1.478A48.567 48.567 0 017.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 013.369 0c1.603.051 2.815 1.387 2.815 2.951zm-6.136-1.452a51.196 51.196 0 013.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 00-6 0v-.113c0-.794.609-1.428 1.364-1.452zm-.355 5.945a.75.75 0 10-1.5.058l.347 9a.75.75 0 101.499-.058l-.346-9zm5.48.058a.75.75 0 10-1.498-.058l-.347 9a.75.75 0 001.5.058l.345-9z"
-  //                 clipRule="evenodd"
-  //               />
-  //             </svg>
-  //           </button>
-  //         </div>
-  //       </div>
-  //     </div>
-  //   );
-  // });
-
-  //Buttons inside the card
-
   const data = wishlistItems.map((item) => {
     return (
       <Link
@@ -189,69 +112,6 @@ const WishListContanier = ({ attr }) => {
     );
   });
 
-  //Table
-  // const data = wishlistItems.map((item, idx) => {
-  //   return (
-  //     <tr
-  //       key={item._id}
-  //       className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 text-center"
-  //     >
-  //       <td className="whitespace-nowrap px-6 py-4 font-medium text-center">
-  //         {idx + 1}
-  //       </td>
-  //       <td className="whitespace-nowrap px-6 py-4 font-medium text-center">
-  //         <img className={wishlistStyle.tableImage} src={item.Images[0]} />
-  //       </td>
-  //       <th
-  //         scope="row"
-  //         className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white text-left"
-  //       >
-  //         {item.name}
-  //       </th>
-  //       <td className="px-6 py-4 text-center">Alexandria</td>
-  //       <td className="px-6 py-4 text-center">Cruse</td>
-  //       <td className="px-6 py-4 text-center">${item.AdultPrice}</td>
-  //       <td className="px-6 py-4  text-centert">
-  //         <div className="flex flex-row justify-center">
-  //           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full flex">
-  //             <svg
-  //               xmlns="http://www.w3.org/2000/svg"
-  //               fill="none"
-  //               viewBox="0 0 24 24"
-  //               strokeWidth="1.5"
-  //               stroke="currentColor"
-  //               className="w-6 h-6"
-  //             >
-  //               <path
-  //                 strokeLinecap="round"
-  //                 strokeLinejoin="round"
-  //                 d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
-  //               />
-  //             </svg>
-  //             <p className="ml-3">Add To Cart</p>
-  //           </button>
-  //         </div>
-  //       </td>
-  //       <td className="px-6 py-4 text-center">
-  //         <button onClick={() => handleRemoveFromWishlist(item)}>
-  //           <svg
-  //             xmlns="http://www.w3.org/2000/svg"
-  //             viewBox="0 0 24 24"
-  //             fill="red"
-  //             className="w-6 h-6"
-  //           >
-  //             <path
-  //               fillRule="evenodd"
-  //               d="M16.5 4.478v.227a48.816 48.816 0 013.878.512.75.75 0 11-.256 1.478l-.209-.035-1.005 13.07a3 3 0 01-2.991 2.77H8.084a3 3 0 01-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 01-.256-1.478A48.567 48.567 0 017.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 013.369 0c1.603.051 2.815 1.387 2.815 2.951zm-6.136-1.452a51.196 51.196 0 013.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 00-6 0v-.113c0-.794.609-1.428 1.364-1.452zm-.355 5.945a.75.75 0 10-1.5.058l.347 9a.75.75 0 101.499-.058l-.346-9zm5.48.058a.75.75 0 10-1.498-.058l-.347 9a.75.75 0 001.5.058l.345-9z"
-  //               clipRule="evenodd"
-  //             />
-  //           </svg>
-  //         </button>
-  //       </td>
-  //     </tr>
-  //   );
-  // });
-
   return (
     <div>
       <div className="flex justify-center items-center mt-10 mb-10">
@@ -276,42 +136,12 @@ const WishListContanier = ({ attr }) => {
           </mark>
         </h1>
       </div>
-      <div className="flex flex-wrap">{data}</div>
-      {/* <div className="content-center flex justify-center items-center mb-36">
-        <div className="relative overflow-x-auto shadow-md sm:rounded-lg w-10/12">
-          <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-              <tr>
-                <th scope="col" className="px-6 py-4 text-center">
-                  #
-                </th>
-                <th scope="col" className="px-6 py-4 text-center">
-                  Img
-                </th>
-                <th scope="col" className="px-6 py-4 text-center">
-                  Name
-                </th>
-                <th scope="col" className="px-6 py-4 text-center">
-                  City
-                </th>
-                <th scope="col" className="px-6 py-4 text-center">
-                  Type
-                </th>
-                <th scope="col" className="px-6 py-4 text-center">
-                  Price
-                </th>
-                <th scope="col" className="px-6 py-4 text-center">
-                  Add To Cart
-                </th>
-                <th scope="col" className="px-6 py-4 text-center">
-                  Delete
-                </th>
-              </tr>
-            </thead>
-            <tbody>{data}</tbody>
-          </table>
+      {/* <div className="flex flex-wrap"> */}
+      <div className="container p-5 mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+          {data}
         </div>
-      </div> */}
+      </div>
     </div>
   );
 };
