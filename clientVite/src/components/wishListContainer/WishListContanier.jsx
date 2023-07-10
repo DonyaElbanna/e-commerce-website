a4cfefad97caeaf56384824140747306d867a779
af5rany <58744028+af5rany@users.noreply.github.com>
2023-07-10 04:16:08 +0300

    wishlist responsive

--- clientVite/src/components/wishListContainer/WishListContanier.jsx
+++ clientVite/src/components/wishListContainer/WishListContanier.jsx
@@ -5,12 +5,13 @@ import wishlistStyle from "./WishListContanier.module.css";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 
-const baseURL = "http://localhost:9999/user/64a0892352ffbc8453b77147";
+// const baseURL = "http://localhost:9999/user/64a0892352ffbc8453b77147";
 
 const WishListContanier = ({ attr }) => {
   const [wishlistItems, setWishlistItems] = useState([]);
-  // const { auth } = useSelector((state) => state);
-  // const baseURL = `http://localhost:9999/user/${auth.userInfo._id}`;
+  console.log(wishlistItems[0]);
+  const { auth } = useSelector((state) => state);
+  const baseURL = `http://localhost:9999/user/${auth.userInfo._id}`;
 
   useEffect(() => {
     const getWishlistItems = async () => {
@@ -22,7 +23,7 @@ const WishListContanier = ({ attr }) => {
   // console.log(wishlistItems);
 
   const handleRemoveFromWishlist = async (event, item) => {
-    event.preventDefault(); ////
+    event.preventDefault();
     const updatedWishlist = wishlistItems.filter(
       (wishlistItem) => wishlistItem._id !== item._id
     );
@@ -36,7 +37,6 @@ const WishListContanier = ({ attr }) => {
   const ddd = (event) => {
     event.preventDefault();
   };
-  console.log(wishlistItems[0]);
   return (
     <div className="container px-5 mx-auto mb-5">
       <div className="flex justify-center items-center mt-10 mb-10">
@@ -69,24 +69,25 @@ const WishListContanier = ({ attr }) => {
           style={{ width: "150px" }}
         />
       ) : (
-        <div className="p-10 grid sm:grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 gap-5">
+        // lg:mx-10
+        <div className="py-10 grid sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 lg:gap-2">
           {wishlistItems.map((cat) => (
             <Link
-              className="ml-5 mr-9 group w-11/12 h-fit"
+              className="group mx-24 md:mx-3 xl:ml-0 h-fit mt-6"
+              // ml-5  mr-9 group w-11/12 h-fit
               key={cat._id}
               to={`/city/${cat._id}/details`}
             >
-              <div className="md:mr-8 group-hover:scale-105  md:w-80 lg:flex">
+              <div className="group-hover:scale-105 xl:ml-0 lg:flex">
                 {/* w-full */}
                 <div
-                  className="h-48 rounded-t-xl lg:rounded-r-none lg:rounded-l-xl lg:h-auto lg:w-48 flex-none bg-cover text-center overflow-hidden"
-                  //  rounded-t  lg:rounded-l rounded-l-xl
+                  className="h-48 rounded-t-xl lg:rounded-r-none lg:rounded-l-xl lg:h-auto xl:w-36 lg:w-44 2xl:w-48 flex-none bg-cover text-center overflow-hidden"
                   style={{
                     backgroundImage: `url('${cat.Images[0]}')`,
                   }}
                 ></div>
-                <div className="border-l group-hover:bg-slate-200 rounded-b-xl lg:rounded-l-none lg:rounded-r-xl  2xl:h-64 border-xxl border-gray-400 sm:border-l lg:border-t border-r border-b lg:border-gray-400 bg-white p-4 flex flex-col justify-between leading-normal">
-                  {/* rounded-b lg:rounded-b-none lg:rounded-r 2xl:rounded-r-xl */}
+                <div className="border-l xl:w-64 group-hover:bg-slate-200 rounded-b-xl lg:rounded-l-none lg:rounded-r-xl xl:h-64 border-gray-400 sm:border-l lg:border-t border-r border-b bg-white p-4 flex flex-col justify-between leading-normal">
+                  {/* rounded-b lg:rounded-b-none lg:rounded-r 2xl:rounded-r-xl border-xxl*/}
                   {/* sm:rounded-b-xl md:rounded-b-none */}
                   <div className="mb-8">
                     <div className="text-gray-900 font-bold text-xl mb-2">
@@ -104,10 +105,10 @@ const WishListContanier = ({ attr }) => {
                     </div> */}
                     <div className="flex justify-between mt-5">
                       <span className="text-xs inline-flex items-center font-bold leading-sm uppercase px-2 py-1 mx-3 bg-green-200 text-[#be853f] rounded-full w-min">
-                        {cat.category.city}
+                        {/* {cat.category.city} */}
                       </span>
                       <span className="text-xs inline-flex items-center font-bold leading-sm uppercase px-3 py-1 bg-blue-200 text-[#be853f] rounded-full w-min">
-                        {cat.subcategory.type}
+                        {/* {cat.subcategory.type} */}
                       </span>
                     </div>
                   </div>
@@ -115,7 +116,7 @@ const WishListContanier = ({ attr }) => {
                     <div className="flex flex-row-reverse text-center justify-center">
                       <button
                         onClick={ddd}
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full flex w-44"
+                        className=" ml-1 mt-1 bg-blue-500 hover:bg-blue-700 text-white font-bold h-9 py-2 px-4 rounded-full flex w-44"
                       >
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
@@ -134,18 +135,24 @@ const WishListContanier = ({ attr }) => {
                         <p className="ml-3 w-32">Add To Cart</p>
                       </button>
                     </div>
+                    <div className=" flex justify-end p-1"></div>
                     <div className="flex flex-row-reverse text-center justify-start">
-                      <button onClick={() => handleRemoveFromWishlist(item)}>
+                      <button className="btn btn-ghost btn-circle">
                         <svg
+                          onClick={(event) =>
+                            handleRemoveFromWishlist(event, cat)
+                          }
                           xmlns="http://www.w3.org/2000/svg"
+                          fill={"#FF0000"}
                           viewBox="0 0 24 24"
-                          fill="red"
+                          strokeWidth={1.5}
+                          stroke="#DC143C"
                           className="w-6 h-6"
                         >
                           <path
-                            fillRule="evenodd"
-                            d="M16.5 4.478v.227a48.816 48.816 0 013.878.512.75.75 0 11-.256 1.478l-.209-.035-1.005 13.07a3 3 0 01-2.991 2.77H8.084a3 3 0 01-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 01-.256-1.478A48.567 48.567 0 017.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 013.369 0c1.603.051 2.815 1.387 2.815 2.951zm-6.136-1.452a51.196 51.196 0 013.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 00-6 0v-.113c0-.794.609-1.428 1.364-1.452zm-.355 5.945a.75.75 0 10-1.5.058l.347 9a.75.75 0 101.499-.058l-.346-9zm5.48.058a.75.75 0 10-1.498-.058l-.347 9a.75.75 0 001.5.058l.345-9z"
-                            clipRule="evenodd"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
                           />
                         </svg>
                       </button>
