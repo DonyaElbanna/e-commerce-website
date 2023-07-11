7962276daa762129b5b07d1bc6e724c129aad7a5
af5rany <58744028+af5rany@users.noreply.github.com>
2023-07-11 04:13:25 +0300

    removing pause

--- clientVite/src/components/HomeContainer/Welome/welcome.jsx
+++ clientVite/src/components/HomeContainer/Welome/welcome.jsx
@@ -7,35 +7,12 @@ import SearchGroup from "./SearchComponent";
 
 function Welcome() {
   // const [cats, setCats] = useState([]);
-  // const Navigate = useNavigate();
 
-  // const handleNavigate = (tar) => {
-  //   Navigate(`/city/${tar}`);
-  // };
-
-  // const toggleVideo = () => {
-  //   if (isPlaying) {
-  //     videoRef.current.pause();
-  //   } else {
-  //     videoRef.current.play();
-  //   }
-  //   setIsPlaying(!isPlaying);
-  // };
-
-  // let play = "";
   return (
     <>
       <section className={Style.container}>
         <div className={Style.video} onContextMenu={(e) => e.preventDefault()}>
-          <video
-            src={video}
-            // ref={videoRef}
-            type="video/mp4"
-            // onClick={toggleVideo}
-            autoPlay
-            loop
-            muted
-          ></video>
+          <video src={video} type="video/mp4" autoPlay loop muted></video>
         </div>
         <div className={Style.filter}>
           {/* <p className="text-lg xs:text-xl sm:text-xl md:text-2xl xl:text-3xl my-2">
