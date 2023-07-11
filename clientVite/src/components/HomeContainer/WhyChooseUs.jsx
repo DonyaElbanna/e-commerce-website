fe6d7e6a24732bb14942be42f9a8267a8aaf7f48
asmaa abdrabo <asmaa90@outlook.com>
2023-07-11 21:32:03 +0300

    why choose us

---
+++ clientVite/src/components/HomeContainer/WhyChooseUs.jsx
@@ -0,0 +1,137 @@
+import React from "react";
+const WhyChooseUs = () => {
+  return (
+    <>
+      <div className="container p-5 mx-auto">
+        <h3 className="text-4xl sm:text-5xl text-center my-16 text-zinc-700 headerFont">
+          Why <span className="text-[#be853f]">Choose</span> Us ?
+        </h3>
+        <div className="container p-5 px-20 mx-auto">
+          <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-20">
+            <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <div className="flex flex-col items-center pb-10">
+                <img
+                  className="w-24 h-24 m-5 rounded-full shadow-lg"
+                  src="https://cdn-icons-png.flaticon.com/512/5346/5346638.png"
+                  alt="Bonnie image"
+                />
+                <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
+                  +1000 tours
+                </h5>
+                <p className="text-[#be853f] text-center px-10">
+                  Incredible experience at unbeatable prices,that is the
+                  Egyption Tickets Tours way
+                </p>
+                <div className="flex mt-4 space-x-3 md:mt-6 px-10 text-center">
+                  <p>
+                    To give you that personalized travel experience, we design
+                    the holiday For You, as you want it.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <div className="flex flex-col items-center pb-10">
+                <img
+                  className="w-24 h-24 m-5 rounded-full shadow-lg"
+                  src="https://www.pngitem.com/pimgs/m/20-202842_safe-online-payments-with-3-d-secure-secure.png"
+                  alt="Bonnie image"
+                />
+                <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
+                  Secure Online Payment
+                </h5>
+                <p className="text-[#be853f] text-center px-10">
+                  With Egyption Tickets Tours your money in safe
+                </p>
+                <div className="flex mt-4 space-x-3 md:mt-6 px-10 text-center">
+                  <p>
+                    Use your debit card or credit card. Your transactions are
+                    protected by 3D Secure and Secure Code.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <div className="flex flex-col items-center pb-10">
+                <img
+                  className="w-24 h-24 m-5 rounded-full shadow-lg"
+                  src="https://icon-library.com/images/call-center-icon-png/call-center-icon-png-22.jpg"
+                  alt="Bonnie image"
+                />
+                <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
+                  helpline Support
+                </h5>
+                <p className="text-[#be853f] text-center px-10">
+                  helpline for all your questions and grievances.
+                </p>
+                <div className="flex mt-4 space-x-3 md:mt-6 px-10 text-center">
+                  <p>
+                    Communication and Information in French, Spanish,
+                    Portuguese, Italian, German ,etc…
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* <div
+          className="wow zoomIn wpb_column col-sm-12 col-md-4 animated"
+          style={{ visibility: "visible", animationName: "zoomIn" }}
+        >
+          <div className="vc_column-inner">
+            <div className="wpb_wrapper">
+              <div className="ct-icon-box  feature_home">
+                <i className="icon_set_1_icon-30"></i>
+                <h3>
+                  <b>+100000</b> Customers
+                </h3>
+                <p className="">
+                  <strong>
+                    Strong offices across the country that ensure to add an
+                    authentic local taste to your holiday.
+                  </strong>
+                </p>
+                <p>&nbsp;</p>
+                <hr />
+                <p>
+                  <strong>
+                    Saving your money by recommending offers, moving a date,
+                    changing a hotel, re-routing!
+                  </strong>
+                </p>
+              </div>
+              <div className="ct-icon-box  feature_home">
+                <i className="icon_set_1_icon-37"></i>
+                <h3>
+                  <span style={{ color: "#33cccc" }}>Personalized</span> Tours
+                </h3>
+                <p>
+                  <strong>
+                    With{" "}
+                    <span style={{ color: "#ff9900" }}>Sun Pyramids Tours</span>{" "}
+                    have privacy
+                  </strong>
+                </p>
+                <p>&nbsp;</p>
+                <hr />
+                <p>
+                  <span style={{ color: "#999999" }}>
+                    <strong>
+                      If you are searching for a private customized tour, this
+                      is the right place where accuracy and proficiency are
+                      guaranteed. Choose the details of your own story now!
+                    </strong>
+                  </span>
+                </p>
+                <h4></h4>
+                <p></p>
+              </div>
+            </div>
+          </div>
+        </div> */}
+      </div>
+    </>
+  );
+};
+
+export default WhyChooseUs;
--- clientVite/src/pages/Home.jsx
+++ clientVite/src/pages/Home.jsx
@@ -1,11 +1,13 @@
-import Welcome from '../components/HomeContainer/Welome/welcome';
-import HighestRated from '../components/HomeContainer/HighestRated/HighestRated'
+import Welcome from "../components/HomeContainer/Welome/welcome";
+import HighestRated from "../components/HomeContainer/HighestRated/HighestRated";
+import WhyChooseUs from "../components/HomeContainer/WhyChooseUs";
 
 const Home = () => {
   return (
     <>
       <Welcome />
       <HighestRated />
+      <WhyChooseUs />
     </>
   );
 };
